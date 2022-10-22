42d6fd3698dc34e714f516db2e60ac17c14976be
xiristian <christianhederson22@gmail.com>
2022-10-22 10:59:31 -0300

    Encrontrar localização atual

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -5,11 +5,12 @@ import {
 import { Button } from "react-native-paper";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer, createNavigationContainerRef } from "@react-navigation/native";
+import * as Location from 'expo-location';
 
 import CadastroPosto from "../components/CadastroPosto";
 import CalculoRota from "../components/CalculoRota";
 import CadastroPessoa from "../components/CadastroPessoa";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { useEffect, useState } from "react";
 
 const StackSheet = createNativeStackNavigator();
@@ -17,14 +18,21 @@ const navigationRef = createNavigationContainerRef();
 
 export default function MapScreen({ navigation, route }) {
   const initialRegion = {
-    latitude: -28.70249,
-    longitude: -49.40550,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    'region': {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.02,
+      longitudeDelta: 0.02,
+    }
+  };
+
+  const initialMark = {
+    latitude: 0,
+    longitude: 0,
   };
 
   const [region, setRegion] = useState(initialRegion);
-  const voltar = () => setRegion(initialRegion);
+  const [mark, setMark] = useState(initialMark);
 
   function onRegionChange(region) {
     setRegion({ region });
@@ -32,14 +40,50 @@ export default function MapScreen({ navigation, route }) {
 
   useEffect(() => {
     if (route.params?.id) {
-      navigationRef.navigate('Avaliação', {id:route.params?.id})
+      navigationRef.navigate('Avaliação', { id: route.params?.id })
     }
   }, [route.params?.id]);
 
+  function setLocationToRegion(location) {
+    let newRegion = {'region':{
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0,
+      longitudeDelta: 0,}
+    };
+
+    let newMark = initialMark;
+
+    newRegion.longitudeDelta = region.region.longitudeDelta;
+    newRegion.latitudeDelta = region.region.latitudeDelta;
+    newRegion.latitude = location.coords.latitude;
+    newRegion.longitude = location.coords.longitude;
+    newMark.latitude = location.coords.latitude;
+    newMark.longitude = location.coords.longitude;
+
+    setRegion(newRegion);
+    setMark(newMark)
+  }
+
+  const setCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Permissão negada, por favor nos permita usar sua localização!');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocationToRegion(location);
+  }
+
+  useEffect(() => {
+    setCurrentLocation();
+  }, [Location]);
+
   return (
     <View style={[styles.container]}>
-      <View style={[styles.formContainer, { borderColor: 'green' }]}>
-        <Button onPress={voltar} >Retorno</Button>
+      <View style={[styles.formContainer]}>
+        <Button onPress={setCurrentLocation} >Retorno</Button>
         <Button onPress={() => navigationRef.navigate('Calculo')} >Calculo</Button>
         <MapView
           region={region}
@@ -47,6 +91,7 @@ export default function MapScreen({ navigation, route }) {
           style={{ height: '100%', width: '100%' }}
           onPoiClick={() => { }}
         >
+          <Marker coordinate={mark}></Marker>
         </MapView>
       </View>
       <View style={[styles.formContainer]}>
@@ -81,8 +126,6 @@ const styles = StyleSheet.create({
   },
   formContainer: {
     flex: 1,
-    borderColor: 'red',
-    borderWidth: 3
   },
   title: {
     fontSize: 24,
